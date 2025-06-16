34723863491e33c811f2aeb8140df51514c94bc7
lokigram <coilaloki87@gmail.com>
2025-06-16 19:20:42 -0400

    modificado en control de versiones android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.animales_domesticos"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077975"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
